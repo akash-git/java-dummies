23ea00ff39e6dbcdce9726ab0ec74914500b572c
akash sharma <akashsharmaa@gmail.com>
2023-07-20 19:08:33 +0530

    adding blocking tests

--- src/main/java/blocking/Test.java
+++ src/main/java/blocking/Test.java
@@ -2,10 +2,12 @@ package blocking;
 
 import blocking.executors.DoubleThreadExecutors;
 import blocking.executors.SingleExecutors;
+import blocking.executors.VirtualThreadsExecutors;
 
 public class Test {
     public static void main(String[] args) {
         new SingleExecutors().start();
         new DoubleThreadExecutors().start();
+        new VirtualThreadsExecutors().start();
     }
 }
